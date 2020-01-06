0356d7e88f13976ff4d0a55bcb5ccbd28fe2098c
Silkov Max <silkov34@yandex.ru>
2020-01-06 17:40:50 +0300

    firstcommit

---
+++ test123.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let areasMoscow = ['Академический', 'Алексеевский', 'Алтуфьевский'];
+let wasINareas = [];
+let tobeAreasMoscow = [];
+
+
+function arrayRandElement(arr) {
+    let rand = Math.floor(Math.random() * arr.length);
+    console.log(arr.length);
+    return arr[rand];
+
+}
+wasINareas.push(arr[rand]);
+
+console.log(arrayRandElement(areasMoscow));
+console.log(areasMoscow);
+console.log(wasINareas);
+tyty
